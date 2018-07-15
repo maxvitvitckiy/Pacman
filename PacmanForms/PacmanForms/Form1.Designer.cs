a8c7a5e655e399b74670feb29668d2c7a099fe3a
wispeckiy <max.vitvitckiy@gmail.com>
2018-07-15 18:28:49 +0300

    pacman moving

--- PacmanForms/PacmanForms/Form1.Designer.cs
+++ PacmanForms/PacmanForms/Form1.Designer.cs
@@ -28,23 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1529, 358);
+            this.ClientSize = new System.Drawing.Size(739, 500);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PACMAN";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- PacmanForms/PacmanForms/Form1.cs
+++ PacmanForms/PacmanForms/Form1.cs
@@ -12,7 +12,6 @@ namespace PacmanForms
 {
     public partial class Form1 : Form
     {
-        Timer t;
         Pacman pacman;
         Graphics g;
 
@@ -22,8 +21,18 @@ namespace PacmanForms
             this.BackColor = Color.Black;
 
             pacman = new Pacman();
-
+            
+            timer1.Interval = 20;
+            timer1.Enabled = true;
             g = this.CreateGraphics();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            pacman.Move();
+            Refresh();
+            g.DrawImage(pacman.GetPacmanImg(), pacman.GetRect());
+
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -31,15 +40,58 @@ namespace PacmanForms
             g.DrawImage(pacman.GetPacmanImg(), pacman.GetRect());
         }
 
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            //if ( == Keys.Right)
+            //{
+            //    pacman.SetVectorX(1);
+            //    pacman.SetVectorY(0);
+            //}
+            //else if (e.KeyChar. == Keys.Left)
+            //{
+            //    pacman.SetVectorX(-1);
+            //    pacman.SetVectorY(0);
+            //}
+            //else if (e.KeyCode == Keys.Down)
+            //{
+            //    pacman.SetVectorX(0);
+            //    pacman.SetVectorY(-1);
+            //}
+
+            //else if (e.KeyCode == Keys.Up)
+            //{
+            //    pacman.SetVectorX(0);
+            //    pacman.SetVectorY(1);
+            //}
+        }
+
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Right)
             {
                 pacman.SetVectorX(1);
+                pacman.SetVectorY(0);
             }
-            pacman.Move();
-            g.DrawImage(pacman.GetPacmanImg(), pacman.GetRect());
+            else if (e.KeyCode == Keys.Left)
+            {
+                pacman.SetVectorX(-1);
+                pacman.SetVectorY(0);
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                pacman.SetVectorX(0);
+                pacman.SetVectorY(-1);
+            }
 
+            else if (e.KeyCode == Keys.Up)
+            {
+                pacman.SetVectorX(0);
+                pacman.SetVectorY(1);
+            }
+            //pacman.Move();
+            //Refresh();
+            //g.DrawImage(pacman.GetPacmanImg(), pacman.GetRect());
         }
 
 
